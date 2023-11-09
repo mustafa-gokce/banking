bf426e54b356485ce1d4d8a8b6fd3f19019ce6d2
mustafa-gokce <mustafagokce@re-science.com>
2023-11-09 21:49:28 +0300

    Added message definitions for account list, balance adding and transaction

--- messages.h
+++ messages.h
@@ -15,6 +15,12 @@ enum class MSG_ID : uint16_t {
     LOGIN_RESPONSE = 5,
     LOGOUT_REQUEST = 6,
     LOGOUT_RESPONSE = 7,
+    ACCOUNT_LIST_REQUEST = 8,
+    ACCOUNT_LIST_RESPONSE = 9,
+    ADD_BALANCE_REQUEST = 10,
+    ADD_BALANCE_RESPONSE = 11,
+    TRANSACTION_REQUEST = 12,
+    TRANSACTION_RESPONSE = 13,
 };
 MSGPACK_ADD_ENUM(MSG_ID)
 
@@ -146,6 +152,100 @@ class BANK_LIST_RESPONSE {
 public:
     std::vector<Bank> banks{};
     MSGPACK_DEFINE (banks);
+};
+
+/*
+ * This is the message that is sent from the client to the server to request a list of IBANs.
+ */
+class ACCOUNT_LIST_REQUEST {
+public:
+    uint32_t user{};
+    std::string token{};
+    uint16_t bank{};
+    MSGPACK_DEFINE (user, token, bank);
+};
+
+/*
+ * This is the message sub-object that is sent from the server to the client in response to a ACCOUNT_LIST_RESPONSE.
+ */
+class Account {
+public:
+    std::string iban{};
+    uint32_t user{};
+    uint16_t bank{};
+    double_t balance{};
+    MSGPACK_DEFINE (iban, user, bank, balance);
+};
+
+/*
+ * This is the message that is sent from the server to the client in response to a ACCOUNT_LIST_REQUEST.
+ */
+class ACCOUNT_LIST_RESPONSE {
+public:
+    std::vector<Account> accounts{};
+    MSGPACK_DEFINE (accounts);
+};
+
+/*
+ * This is the message that is sent from the client to the server to add balance to an IBAN as a superuser.
+ */
+class ADD_BALANCE_REQUEST {
+public:
+    std::string user{};
+    std::string token{};
+    std::string iban{};
+    double_t amount{};
+    MSGPACK_DEFINE (user, token, iban, amount);
+};
+
+/*
+ * This is the message that is sent from the server to the client in response to an ADD_BALANCE_REQUEST.
+ * Inheriting from ADD_BALANCE_REQUEST is a hack to reuse the MSGPACK_DEFINE macro.
+ * Echo back the total balance of the account after adding the amount.
+ */
+class ADD_BALANCE_RESPONSE : public ADD_BALANCE_REQUEST {
+public:
+    MSGPACK_DEFINE ();
+};
+
+/*
+ * This is the message that is sent from the client to the server to request a transaction.
+ */
+class TRANSACTION_REQUEST {
+public:
+    std::string user{};
+    std::string token{};
+    std::string from{};
+    std::string to{};
+    double_t amount{};
+    MSGPACK_DEFINE (user, token, from, to, amount);
+};
+
+/*
+ * This is a list of all the transaction response types.
+ */
+enum class TRANSACTION_RESPONSE_TYPE : uint8_t {
+    TRANSACTION_SUCCESS = 0,
+    SERVER_ERROR = 1,
+    NOT_LOGGED_IN = 2,
+    INVALID_TOKEN = 3,
+    INVALID_FROM_IBAN = 4,
+    INVALID_TO_IBAN = 5,
+    INVALID_AMOUNT = 6,
+    INSUFFICIENT_FUNDS = 7,
+    UNKNOWN = 255,
+};
+MSGPACK_ADD_ENUM(TRANSACTION_RESPONSE_TYPE)
+
+/*
+ * This is the message that is sent from the server to the client in response to a TRANSACTION_REQUEST.
+ */
+class TRANSACTION_RESPONSE {
+public:
+    TRANSACTION_RESPONSE_TYPE type{TRANSACTION_RESPONSE_TYPE::UNKNOWN};
+    std::string token{};
+    float_t fee{};
+    MSGPACK_DEFINE (type, token, fee);
 };
 
 #endif //BANKING_MESSAGES_H
