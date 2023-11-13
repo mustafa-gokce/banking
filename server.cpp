d8d3c5a05996ad746be2e58ac7e00875e0f21add
mustafa-gokce <mustafagokce@re-science.com>
2023-11-13 15:44:58 +0300

    Server waits for Ctrl+C from user to exit

--- server.cpp
+++ server.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <csignal>
 #include "src/Server.h"
 
+// global variable to stop the server
+volatile sig_atomic_t stop;
+
+// signal signal_handler
+void signal_handler(int signal_number) {
+    stop = 1;
+    std::cout << "[server] signal " << signal_number << " received" << std::endl;
+}
+
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
+
+    // register signal SIGINT and signal handler
+    signal(SIGINT, signal_handler);
 
     // create server
     Server::Server server;
@@ -12,7 +25,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
     server.initialize("tcp://127.0.0.1:2609");
 
     // infinite loop
-    while (true) {
+    while (!stop) {
 
         // handle incoming requests
         if (!server.handle_request()){
